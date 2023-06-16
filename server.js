aa09eef590301164dbb3115fe0131c022efd3a15
anisimnida <assatakiya@gmail.com>
2023-06-16 12:06:59 +0700

    2100016021

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./src/routes');
+
+// Set up the Express app
+const app = express();
+const port = 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://regina2100016083:Gina0110@cluster0.vacfo18.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/', routes);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
